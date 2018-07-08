c99cd15b117a107a6cd5375798e1d5b6c1091a21
Earthling <Earthling@EARTHLING-PC>
2018-07-07 21:22:22 -0700

    UserId on ShoppingCartItems now matches user login ID. Button that links to unique shopping cart now only shows when user is logged in too ^_^!

--- CSharpProjectWAccounts/Models/ShoppingCartItem.cs
+++ CSharpProjectWAccounts/Models/ShoppingCartItem.cs
@@ -1,11 +1,30 @@
 ﻿
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System.Web;
+
 namespace CSharpProjectWAccounts.Models
 {
     public class ShoppingCartItem
     {
         public int Id { get; set; }
         public int ItemId { get; set; }
-		public int UserId {get; set;}
+        public string UserId
+        {
+            get
+            {
+                ApplicationUser user = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(System.Web.HttpContext.Current.User.Identity.GetUserId());
+                if(user != null)
+                {
+                    return user.Id;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            set { }
+        }
         public virtual Items Item { get; set; }
     }
 }
